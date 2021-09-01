f096baaffdbf9e7b6acf48bc5e7e3004b89e0952
Des Hartman <des@deshartman.com>
2021-09-01 10:30:24 +1000

    renaming and cleaning up

--- agent-pay-client/src/sdk/payClient.js
+++ agent-pay-client/src/sdk/payClient.js
@@ -74,7 +74,7 @@ const PayClient = {
             //console.log('Axios config' + JSON.stringify(axios_config, null, 4));
             // Update Axios and status call back
             this._twilioAPI = axios.create(axios_config);
-            this._statusCallback = this._config.data.callHandlerURL + '/pay/paySyncUpdate';
+            this._statusCallback = this._config.data.functionsURL + '/pay/paySyncUpdate';
             this.payConnector = this._config.data.payConnector;
             this.captureOrder = this._config.data.captureOrder.slice(); // copy by value TODO: Can probably remove this, since CaptureToken sets it anyway
             this.currency = this._config.data.currency;
@@ -108,7 +108,7 @@ const PayClient = {
             twilioAccountSid: twilioAccountSid,
             twilioApiKey: twilioApiKey,
             twilioApiSecret: twilioApiSecret,
-            callHandlerURL: callHandlerURL,     // The Twilio Functions URL where the call handlers are deployed
+            functionsURL: functionsURL,     // The Twilio Functions URL where the call handlers are deployed
             payConnector: payConnector,         // The name of the Twilio Pay connector configured
             captureOrder: [                     // The order in which the components will be captured
                 "payment-card-number",
