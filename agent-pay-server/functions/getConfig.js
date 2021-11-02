05baa172d99720320a67a0c7adaec98629704719
Des Hartman <des@deshartman.com>
2021-11-03 10:07:15 +1100

    updated Functions to add CORS

--- agent-pay-server/functions/getConfig.js
+++ agent-pay-server/functions/getConfig.js
@@ -9,6 +9,15 @@ exports.handler = async function (context, event, callback) {
 
     console.log(`event: ${JSON.stringify(event, null, 4)}`);
 
+    function sendResponse(data) {
+        const response = new Twilio.Response();
+        response.appendHeader("Access-Control-Allow-Origin", "*");
+        response.appendHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+        response.appendHeader("Content-Type", "application/json");
+        response.setBody(data);
+        return response;
+    }
+
     // 
     // This is the most critical part of your backend code, as you must identify the user and (possibly)
     // challenge them with some authentication scheme. To determine the identity, you might use:
@@ -56,6 +65,6 @@ exports.handler = async function (context, event, callback) {
 
     console.log(`Pay Connector: ${config.paymentConnector}`);
 
-    callback(null, config);
+    callback(null, sendResponse(config));
 
 };
