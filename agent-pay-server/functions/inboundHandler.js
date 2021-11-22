eb802d5c2e0f2fc57764bb67a26c5f3e1b6d9d01
Des Hartman <des@deshartman.com>
2021-11-22 16:33:44 +1100

    added syncmap checks

--- agent-pay-server/functions/inboundHandler.js
+++ agent-pay-server/functions/inboundHandler.js
@@ -1,39 +1,82 @@
 /**
- * This is the inbound from PSTN voice handler. Call PBX SIP number, adding callSID as the UUI 
+ * This is the inbound from PSTN voice handler. Call PBX SIP number, adding callSID as the UUI
+ * Nothing is written to the sync outboundCallMap, since the UUI is the callSID
+ * 
  */
 exports.handler = async function (context, event, callback) {
 
     const restClient = context.getTwilioClient();
 
+    console.log(`Inbound handler. Service Sid: ${context.PAY_SYNC_SERVICE_SID} with Call Sid: ${event.CallSid} `);
+
     // Write the incoming PSTN call's Call SID as the UUI into Sync
     try {
-        // Write the callSID and UUI into Call Map
-        const syncMapItem = await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
-            .syncMaps('uuiMap')
-            .syncMapItems
-            .create({
-                key: event.CallSid,
-                data: {
-                    "uui": event.CallSid,
-                    "pstn-sid": event.CallSid
-                },
-                ttl: 43200  // 12 hours
-            });
+        // Since the uuiMap may not yet exist, we need to update it under a try/catch. If it does not exist, create and then add item
+        try {
+            // Write the callSID and UUI into outboundCall Map
+            const syncMapItem = await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
+                .syncMaps('uuiMap')
+                .syncMapItems
+                .create({
+                    key: event.CallSid,
+                    data: {
+                        "uui": event.CallSid,
+                        "pstn-sid": event.CallSid
+                    },
+                    ttl: 43200  // 12 hours
+                });
+        } catch (error) {
+            // SyncMap uuiMap does not exist, so create it
+            const uuiSyncMap = await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
+                .syncMaps({ uniqueName: 'uuiMap' })
+                .create();
+        } finally {
+            // Create the syncMapItem
+            const syncMapItem = await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
+                .syncMaps('uuiMap')
+                .syncMapItems
+                .create({
+                    key: event.CallSid,
+                    data: {
+                        "uui": event.CallSid,
+                        "pstn-sid": event.CallSid
+                    },
+                    ttl: 43200  // 12 hours
+                });
 
-        // Now make the call to the endpoint. Comment out what is not needed
-        const voiceResponse = new Twilio.twiml.VoiceResponse();
+            /**
+         * Now make the call to the endpoint. Comment out what is not needed
+         * 1) Registered SIP user
+         * 2) Client endpoint
+         * 3) SIP Trunk with domain
+        */
+            const voiceResponse = new Twilio.twiml.VoiceResponse();
 
-        /**
-         * Send the call out to the configured domain as is, adding the User-to-User Identifier (UUI)
-         * -> +number@SIP_DOMAIN?User-to-User=CAxxxxx
-         */
-        const sipTo = event.To + '@' + context.SIP_DOMAIN + '?' + 'User-to-User=' + event.CallSid;
+            /**
+             * 1) Registered SIP endpoint:
+             * Convention is to use the twilio number as the username, including the "+", to register and the SIP domain as required
+             * This allows us to use the same code for registered users and SIP trunks
+             * 
+             * 3) SIP Trunk
+             * Send the call out to the configured domain as is.
+             * 
+             * -> +number@SIP_DOMAIN?User-to-User=CAxxxxx
+             */
+            // Send to SIP user -> sip:+number@SIP_DOMAIN?User-to-User=CAxxxxx
+            const sipTo = event.To + '@' + context.SIP_DOMAIN + '?' + 'User-to-User=' + event.CallSid;
 
-        //console.log(`Dialing ${sipTo}`);
-        voiceResponse.dial().sip(sipTo);
+            // Dial SIP URL
+            console.log(`Dialing ${sipTo}`);
+            voiceResponse.dial().sip(sipTo);
 
-        callback(null, voiceResponse);
+            /**
+             * 2) WebRTC Client TODO: Add example code here
+             */
+
+            callback(null, voiceResponse);
+        }
     } catch (error) {
+        // Some other error occurred
         callback(error, null);
     }
 };
--- agent-pay-server/functions/outboundSyncUpdate.js
+++ agent-pay-server/functions/outboundSyncUpdate.js
@@ -1,30 +1,36 @@
 /**
- * This is the StatusCallback where the outboundHandler POSTs to. Once we receive a status callback, we now update the
- * UUI Sync Map with Call SID with the data we received:
+ * This is the StatusCallback where the outboundHandler POSTs to.
+ * Once we receive a status callback, we now update the GUID Sync Map with CallSID with the data we received:
  * 
  * 1) Extract UUI value from PBX call leg (parent Call SID)
- * 2) Write into UUI sync Map
+ * 2) Write into GUID sync Map
  *
- * call UUI: a UUI from the PBX endpoint, which links to the Call SID eventually for outbound calls
+ * callGUID: a GUID from the PBX endpoint, which links to the CallSID eventually for outbound calls
  * 
  */
 exports.handler = async function (context, event, callback) {
-  //console.log(`Call SID: ${event.CallSid}`);
+  //console.log(`Inside outboundSyncUpdate`);
+  console.log(`Call SID: ${event.CallSid}`);
 
   const restClient = context.getTwilioClient();
 
   try {
     //console.log(`Use ParentCallSid to get UUI`);
     var parentCall = await restClient.calls(event.ParentCallSid).fetch();
-    console.log(`parentCall: ${JSON.stringify(parentCall, null, 4)}`);
+    //console.log(`parentCall: ${JSON.stringify(parentCall, null, 4)}`);
 
-    // Split to get the UUI parameter
+    ///////////////////////// TEMP CODE /////////////////////////////
+    parentCall.to = 'sip:+61401277115@example.com?User-to-User=' + Math.round(Math.random() * 1000000000); // TODO: Test with UUI. This is temp to test
+    //////////////////////////////////////////////////////
+    //console.log(`parentCall To: ${parentCall.to}`);
+
     const paramPart = parentCall.to.split("?")[1];
     const params = new URLSearchParams(paramPart);
     const uui = params.get('User-to-User');
 
-    // If we have a UUI, then we can update the UUI Sync Map
     if (uui) {
+      // Write the CallSid into Sync
+      // Since the uuiMap may not yet exist, we need to update it under a try/catch. If it does not exist, create and then add item
       try {
         await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
           .syncMaps('uuiMap')
@@ -32,21 +38,39 @@ exports.handler = async function (context, event, callback) {
           .create({
             key: uui,
             data: {
-              "uui": uui,
-              "pstn-sid": event.CallSid,
-              "pbx-sid": parentCall.sid,
+              "uui": uui,    //Check param. Might be URLEncoded
+              "pstn-sid": event.CallSid
             },
-            ttl: 43200  // 12 hours
+            ttl: 86400
           });
-        callback(null, uui);
       } catch (error) {
-        callback(error, null);
+        // SyncMap uuiMap does not exist, so create it
+        const uuiSyncMap = await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
+          .syncMaps({ uniqueName: 'uuiMap' })
+          .create();
+      }
+      finally {
+        // Create the syncMapItem
+        await restClient.sync.services(context.PAY_SYNC_SERVICE_SID)
+          .syncMaps('uuiMap')
+          .syncMapItems
+          .create({
+            key: uui,
+            data: {
+              "uui": uui,    //Check param. Might be URLEncoded
+              "pstn-sid": event.CallSid
+            },
+            ttl: 86400
+          });
+        //console.log(`Done with outboundSyncUpdate`);
+        callback(null, uui);
       }
     } else {
       //console.error(`Cannot extract UUI from call, so cannot establish Pay ${error}`);
       callback('Cannot extract UUI from call, so cannot establish Pay', null);
     }
   } catch (error) {
+    // Some other error occurred
     callback(error, null);
   }
 };
