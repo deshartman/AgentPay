b5547495ad00fecc3bbc42b7b085d01c6b865794
Des Hartman <des@deshartman.com>
2022-06-02 18:06:06 +1000

    reverted to btoa

--- agent-pay-client/src/components/LoginTwilio.svelte
+++ agent-pay-client/src/components/LoginTwilio.svelte
@@ -19,12 +19,9 @@
     waiting = true;
 
     // New Authorization Header for username and password
-    const encodedBase64Token = Buffer.from(`${identity}:${password}`).toString("base64");
-    const authorization = `Basic ${encodedBase64Token}`;
-
     const headers = new Headers({
       method: "POST",
-      Authorization: authorization,
+      Authorization: "Basic " + btoa(identity + ":" + password),
     });
     //console.log(`headers: ${JSON.stringify(headers, null, 4)}`);
 
